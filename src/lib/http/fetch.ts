ab0b753f28c37d0cccd956ccd3edc47ae9c2356f
Flysky12138 <xhp443@gmail.com>
2025-04-14 17:19:49 +0800

    refactor: fetch.ts

--- src/lib/http/fetch.ts
+++ src/lib/http/fetch.ts
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'es-toolkit'
 import { toast } from 'sonner'
 import { HEADER } from '../constants'
 import { AesGcm } from '../crypto/aes-gcm'
@@ -18,18 +19,18 @@ const Core = async (promise: () => Promise<Response>) => {
 
     if (!res.ok) throw new Error(`error code ${res.status}`)
 
+    // text
+    if (contentType?.includes('text')) return res.text()
+
     // CustomResponse.encrypt 加密
     const ivJwk = res.headers.get(HEADER.AES_GCM_IVJWK)
     if (ivJwk && contentType?.includes('application/octet-stream')) {
       const buffer = await res.arrayBuffer()
-      return await AesGcm.decrypt(buffer, ivJwk)
+      return AesGcm.decrypt(buffer, ivJwk)
     }
 
-    // text
-    if (contentType?.includes('text')) return await res.text()
-
     // blob
-    return await res.blob()
+    return res.blob()
   } catch (error) {
     console.error(error)
     const message = JSON.parse((error as Error).message)
@@ -46,17 +47,23 @@ const Core = async (promise: () => Promise<Response>) => {
 /**
  * 封装的基础请求方法
  */
-export const CustomFetch = async <T = any>(input: RequestInfo | URL, { body, headers, ...init }: FetchOptions = {}): Promise<T> => {
-  headers = Object.assign({}, headers) // 避免修改原对象
+export const CustomFetch = async <T = any>(input: RequestInfo | URL, { body, headers = {}, ...init }: FetchOptions = {}): Promise<T> => {
+  const clonedHeaders = cloneDeep(headers) // 避免修改原对象
 
   if (!(body instanceof FormData)) {
-    if (!headers['Content-Type']) headers['Content-Type'] = 'application/json'
-    if (headers['Content-Type'].includes('application/json')) body = JSON.stringify(body)
+    if (!clonedHeaders['Content-Type']) {
+      clonedHeaders['Content-Type'] = 'application/json'
+    }
+    if (clonedHeaders['Content-Type'].includes('application/json')) {
+      body = JSON.stringify(body)
+    }
   }
 
   if (typeof input == 'string') {
     input = input.replace(/(?<!:)\/{2,}/g, '/')
-    if (input.endsWith('/')) input = input.slice(0, -1)
+    if (input.endsWith('/')) {
+      input = input.slice(0, -1)
+    }
   }
 
   return Core(async () => await fetch(input, { body, headers, ...init }))
