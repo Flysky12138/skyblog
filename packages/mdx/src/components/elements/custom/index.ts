38787e3144edd8171204518961240a9a510c9be7
Flysky <xhp443@gmail.com>
2026-07-19 05:39:49 +0800

    feat: remove mdx

--- packages/mdx/src/components/elements/custom/index.ts
+++
@@ -1,9 +0,0 @@
-import { MDXComponents } from 'next-mdx-remote-client'
-
-import { Alert } from './alert'
-import { Tabs } from './tabs'
-
-export const custom = {
-  alert: Alert,
-  tabs: Tabs
-} satisfies MDXComponents
